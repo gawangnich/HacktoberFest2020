99e8666c2556939ff513b85644f36debed823dc3
capriz <43799202+capriz@users.noreply.github.com>
2020-10-01 12:42:17 +0800

    Create greatest_common_divisor.py
    Author: Capriz Trenuela
    GitHub: https://github.com/capriz

---
+++ Resources/Python/greatest_common_divisor.py
@@ -0,0 +1,14 @@
+def gcd(x, y):
+    gcd = 1
+    
+    if x % y == 0:
+        return y
+    
+    for k in range(int(y / 2), 0, -1):
+        if x % k == 0 and y % k == 0:
+            gcd = k
+            break  
+    return gcd
+
+print(gcd(12, 17))
+print(gcd(4, 6))
