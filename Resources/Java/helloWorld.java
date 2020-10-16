ab37b793d8666c0d18c0d5be1a46c2ff0a9bc11a
Ashan Tharindu <ashh.tt99@gmail.com>
2020-10-16 13:19:57 +0530

    Add java Resource

---
+++ Resources/Java/helloWorld.java
@@ -0,0 +1,7 @@
+package Resources.Java;
+
+class helloWorld {     //add your classname with your file name
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");       //print your hello world application here
+    }
+}
