a7c6e1de4070c7210857e5443b18e14f3c0d1dae
Milinda Ranawaka <milindaranawaka@gmail.com>
2020-10-22 21:19:06 +0530

    Java Coding Standards Added for Time Calculator

--- Resources/Java/TimeCal.java
+++ Resources/Java/TimeCal.java
@@ -1,54 +1,51 @@
 import java.util.Scanner;
-import java.lang.Exception;
 
-public class TimeCal{
-    public static void main(String args[]){
+public class TimeCal {
+    public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
 
-        int hr=0;
-        int min=0;
-        int sec=0;
+        int hr = 0;
+        int min = 0;
+        int sec = 0;
 
-        while(true){
+        while (true) {
             System.out.println("Enter . to stop");
             System.out.println("Input Format: HR:MIN:SEC");
             String str = scan.nextLine();
             String[] split = str.split(":");
 
-            if(str.equals(".")){
+            if (str.equals(".")) {
                 break;
             }
 
-            try{
-                sec+=Integer.parseInt(split[2]);
-            }
-            catch(ArrayIndexOutOfBoundsException e){
+            try {
+                sec += Integer.parseInt(split[2]);
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Error");
                 System.out.println("Include Hours Also");
                 System.out.println("Ex:  00:12:42");
                 return;
             }
 
-            if(sec>60){
+            if (sec > 60) {
                 min++;
-                sec-=60;
+                sec -= 60;
             }
 
-            min+=Integer.parseInt(split[1]);
+            min += Integer.parseInt(split[1]);
 
-            if(min>60){
+            if (min > 60) {
                 hr++;
-                min-=60;
+                min -= 60;
             }
 
-            hr+=Integer.parseInt(split[0]);
+            hr += Integer.parseInt(split[0]);
 
             System.out.println("Hours\tMinute\tSeconds");
-            System.out.println(hr+"\t"+min+"\t"+sec);
+            System.out.println(hr + "\t" + min + "\t" + sec);
             System.out.println();
 
-
-            if(str.equals(".")){
+            if (str.equals(".")) {
                 break;
             }
         }
@@ -57,6 +54,6 @@ public class TimeCal{
         System.out.println();
         System.out.println("Final Result");
         System.out.println("Hours\tMinute\tSeconds");
-        System.out.println(hr+"\t"+min+"\t"+sec);
+        System.out.println(hr + "\t" + min + "\t" + sec);
     }
 }
